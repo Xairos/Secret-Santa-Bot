c97763f0dd9342a03ce461c940ca055956a8c75d
gregorsharpe <gregorbsharpe@example.com>
2016-11-30 16:03:15 -0500

    SMTP support added in output module, implemented getters for firstname and email attributes. Added printed status messages in the main loop along with a variable to enable/disable them.

--- output.py
+++ output.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 from abc import ABCMeta, abstractmethod
+import smtplib
 
 class Output:
 	__metaclass__ = ABCMeta
-	
+
 	@abstractmethod
 	def process(self, playerPairs):
 		pass
@@ -19,5 +20,26 @@ class SMTP(Output):
 		self.password = password
 
 	def process(self, playerPairs):
-		# Implement me!
-		pass
+		subject = "It\'s beginning to feel a lot like Christmas!"
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.ehlo()
+		server.starttls()
+		server.login(self.email, self.password)
+		for gifter,receiver in playerPairs:
+			if ((gifter.getemail() == None) or (gifter.getemail() == "None")):
+				print("WARN: No email listed for " + gifter.getname())
+				continue
+			content = "\r\n".join([
+  				"From: " + self.email,
+  				"To: " + gifter.getemail(),
+  				"Subject: " + subject,
+  				"",
+  				"Hey " + gifter.getfirstname() + ", it\'s that time of year again!",
+				"",
+				"You are " + receiver.getname() + "\'s secret santa!",
+				"",
+				"Good luck and Happy Holidays!",
+				"Santa\'s Secret Santa Secret Service (S.S.S.S.S)"
+  			])
+			server.sendmail(self.email, gifter.getemail(), content)
+		server.quit()
